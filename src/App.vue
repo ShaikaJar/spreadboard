df3946d4816a5925a591e36ee9bb5d3d803c18e7
lorenz <lorenz@karolo.de>
2022-11-22 21:16:53 +0100

    Pages-Build

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="spreadBoard">
+  <div style="display: flex; flex-flow: column">
     <div class="editor">
-      <ReteEditor />
-      <ReteDock />
-    </div>
-
-    <div class="output-view-container">
-      <div class="output-view">
-        Test
+      <div class="editor-container">
+        <ReteEditor />
       </div>
+      <div class="output-view-container">
+        <div class="output-view">
+          Test
+        </div>
 
-      <button @click="triggDownload()">Download</button>
+        <button @click="triggDownload()">Download</button>
+      </div>
     </div>
+    <ReteDock/>
   </div>
 </template>
 
@@ -42,31 +43,26 @@ body{
 
   background: #2c3e50;
 }
-.spreadBoard{
-  display: flex;
-  flex-flow: row;
-  align-items: flex-start;
+
+.editor-container{
+  flex: 2;
+  height: 60vh;
 }
 .editor {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 25px;
-  width: 60vw;
-  height: 90vh;
+  max-width: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: start;
   align-items: flex-start;
-  flex-flow: column-reverse;
+  flex-flow: row;
 
   flex: 1;
-  order: 1;
 }
-.node .control input, .node .input-control input {
-  width: 90%;
-}
+
 select, input {
   width: 100%;
   border-radius: 30px;
@@ -74,7 +70,6 @@ select, input {
   padding: 2px 6px;
   border: 1px solid #999;
   font-size: 110%;
-  width: 170px;
 }
 .output-view-container{
   flex: 1;
@@ -83,9 +78,13 @@ select, input {
   max-width: 50vh;
 }
 .output-view{
+  align-content: start;
+  alignment: unset;
+  justify-content: unset;
+  color: white;
   padding: 5px;
-  max-width: 50vh;
-  height: 80vh;
+  width: 30vh;
+  height: 50vh;
   border-radius: 5px;
   border: 3px solid black;
   margin: 5px;
